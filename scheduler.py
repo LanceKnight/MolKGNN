9298f2e92e990daebc9d9ebe2cdf321cb8cea0da
LanceKnight <lanceknight26@gmail.com>
2022-05-02 12:34:13 -0500

    update wrapper to only include 0.8 train and 0.2 testing

--- scheduler.py
+++ scheduler.py
@@ -9,93 +9,125 @@ import time
 
 
 def gitclone(dir_name):
-	cwd = os.getcwd()
-	os.chdir(dir_name)
-	os.system('git clone git@github.com:LanceKnight/kgnn.git')
-	os.chdir('kgnn')
-	os.system('git checkout add-testing-set') # Change this
-	os.chdir(cwd)
+    cwd = os.getcwd()
+    os.chdir(dir_name)
+    os.system('git clone git@github.com:LanceKnight/kgnn.git')
+    os.chdir('kgnn')
+    os.system('git checkout bcl-benchmark') # Change this
+    os.chdir(cwd)
 
 def gitupdate(dir_name):
-	cwd = os.getcwd()
-	os.chdir(dir_name+'/kgnn')
-	os.system('git checkout add-testing-set')
-	os.system('git pull')
-	os.chdir(cwd)
+    cwd = os.getcwd()
+    os.chdir(dir_name+'/kgnn')
+    os.system('git gc')
+    os.system('git checkout bcl-benchmark') # Change this
+    os.system('git pull')
+    os.chdir(cwd)
 
-def run_command(exp_id, warmup, peak_lr, end_lr, num_layers):
-	# Model=kgnn
-	os.system(f'python entry.py \
-		--task_name {exp_id}\
-		--dataset_name 435034 \
-		--num_workers 16 \
-		--dataset_path ../../../dataset/ \
-		--enable_oversampling_with_replacement \
-		--warmup_iterations {warmup} \
-		--max_epochs 20 \
-		--peak_lr {peak_lr} \
-		--end_lr {end_lr} \
-		--batch_size 17 \
-		--default_root_dir actual_training_checkpoints \
-		--gpus 1 \
-		--num_layers {num_layers} \
-		--num_kernel1_1hop 10 \
-		--num_kernel2_1hop 20 \
-		--num_kernel3_1hop 30 \
-		--num_kernel4_1hop 50 \
-		--num_kernel1_Nhop 10 \
-		--num_kernel2_Nhop 20 \
-		--num_kernel3_Nhop 30 \
-		--num_kernel4_Nhop 50 \
-		--node_feature_dim 27 \
-		--edge_feature_dim 7 \
-		--hidden_dim 64')\
+def run_command(exp_id, dataset, num_layers): # Change this
+    # Model=kgnn
+    os.system(f'python entry.py \
+        --task_name {exp_id}\
+        --dataset_name {dataset} \
+        --num_workers 16 \
+        --dataset_path ../../../dataset/ \
+        --enable_oversampling_with_replacement \
+        --warmup_iterations 200 \
+        --max_epochs 20 \
+        --peak_lr 5e-2 \
+        --end_lr 1e-9 \
+        --batch_size 17 \
+        --default_root_dir actual_training_checkpoints \
+        --gpus 1 \
+        --num_layers {num_layers} \
+        --num_kernel1_1hop 10 \
+        --num_kernel2_1hop 20 \
+        --num_kernel3_1hop 30 \
+        --num_kernel4_1hop 50 \
+        --num_kernel1_Nhop 10 \
+        --num_kernel2_Nhop 20 \
+        --num_kernel3_Nhop 30 \
+        --num_kernel4_Nhop 50 \
+        --node_feature_dim 27 \
+        --edge_feature_dim 7 \
+        --hidden_dim 64')\
 
+def copyanything(src, dst):
+    # If dst exits, remove it first
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    try:
+        shutil.copytree(src, dst)
+    except OSError as exc: # python >2.5
+        if exc.errno in (errno.ENOTDIR, errno.EINVAL):
+            shutil.copy(src, dst)
+        else: raise
 
-def run(exp_id, warmup, peak_lr, end_lr, num_layers):
-	exp_name = f'exp{exp_id}_warmup_{warmup}_peak_{peak_lr}_end_{end_lr}_layers_{num_layers}'
-	print(f'running {exp_name}')
+def run(exp_id, dataset, num_layers):
+    exp_name = f'exp{exp_id}_dataset{dataset}_layers_{num_layers}' # Change this
+    print(f'=====running {exp_name}')
 
-	# Go to correct folder
-	dir_name = f'../experiments/{exp_name}' # Change this
-	if not os.path.exists(dir_name):
-		os.mkdir(dir_name)
-		gitclone(dir_name)
-	gitupdate(dir_name)
-	cwd = os.getcwd()
-	os.chdir(dir_name+'/kgnn')
+    # Go to correct folder
+    dir_name = f'../experiments/{exp_name}' 
+    # if not os.path.exists(dir_name):
+    #   os.mkdir(dir_name)
+    #   gitclone(dir_name)
+    # gitupdate(dir_name)
 
-	# # Task
-	run_command(exp_id, warmup, peak_lr, end_lr, num_layers)
-	# time.sleep(2)
-	os.chdir(cwd)
-	
+    global github_repo_dir
+    copyanything(github_repo_dir, dir_name)
+    cwd = os.getcwd()
+    os.chdir(dir_name+'/kgnn')
+
+    # # Task
+    run_command(exp_id, dataset, num_layers) # Change this
+    # time.sleep(3)
+    print(f'----{exp_name} finishes')
+    os.chdir(cwd)
+    
 
 def attach_exp_id(input_tuple, tuple_id):
-	record = [tuple_id]
+    # Add experiment id in front of the input hyperparam tuple
+    record = [tuple_id]
+    record.extend(list(input_tuple))
+    return record
 
-	record.extend(list(input_tuple))
-	
-	return record
+
+# Global variable
+# Github repo template
+github_repo_dir = f'../experiments/exp_template_dataset_layers'# Change this
 
 if __name__ == '__main__':
-	mp.set_start_method('spawn')
+    mp.set_start_method('spawn')
 
-	warmup = [200, 2000, 20000]
-	# num_epochs = [10, 20, 50]q
-	peak_lr = [5e-1, 5e-2, 5e-3]
-	end_lr = [1e-8, 1e-9, 1e-10]
-	num_layers = [3, 4, 5]
+    # Change this
+    # Hyperparms
+    dataset_list = ['435008', '1798', '435034', '1843', '2258', '463087', '488997','2689', '485290'] 
+    # warmup = [200, 2000, 20000]
+    # # num_epochs = [10, 20, 50]q
+    # peak_lr = [5e-1, 5e-2, 5e-3]
+    # end_lr = [1e-8, 1e-9, 1e-10]
+    num_layers = [3, 4, 5]
+    data_pair = list(itertools.product(dataset_list, num_layers))
+    print(f'num data_pair:{len(data_pair)}')
+    data_pair_with_exp_id = list(map(attach_exp_id, data_pair, range(len(data_pair))))
+    print(f'data_pair_with_exp_id:{data_pair_with_exp_id}')
+    with open('scheduler.log', "w+") as out_file:
+        out_file.write(f'num data_pair:{len(data_pair)}')
+        out_file.write(f'data_pair_with_exp_id:{data_pair_with_exp_id}')
 
-	data_pair = list(itertools.product(warmup, peak_lr, end_lr, num_layers))
-	print(f'num data_pair:{len(data_pair)}')
-	data_pair_with_exp_id = map(attach_exp_id, data_pair, range(len(data_pair)))
-	print(f'data_pair_with_exp_id:{data_pair_with_exp_id}')
 
-	
-	with Pool(processes = 5) as pool:
-		pool.starmap(run, data_pair_with_exp_id)
+    # Clone once from github
+    
+    if not os.path.exists(github_repo_dir):
+        os.mkdir(github_repo_dir)
+        gitclone(github_repo_dir)
+    gitupdate(github_repo_dir)
 
-	pool.join()
-	print(f'finish')
+    
+    with Pool(processes = 5) as pool:
+        pool.starmap(run, data_pair_with_exp_id)
+
+    pool.join()
+    print(f'finish')
 
--- wrapper.py
+++ wrapper.py
@@ -670,10 +670,10 @@ class QSARDataset(InMemoryDataset):
 
         num_active_train = round(num_active * 0.8)
         num_inactive_train = round(num_inactive * 0.8)
-        num_active_valid = round(num_active * 0.1)
-        num_inactive_valid = round(num_inactive * 0.1)
-        num_active_test = round(num_active * 0.1)
-        num_inactive_test = round(num_inactive * 0.1)
+        num_active_valid = round(num_active * 0.2)
+        num_inactive_valid = round(num_inactive * 0.2)
+        # num_active_test = round(num_active * 0.1)
+        # num_inactive_test = round(num_inactive * 0.1)
         # print(f'wrapper.py::num_active_train:{num_active_train} '
         #       f'num_inactive_train:{num_inactive_train}')
         # print(f'wrapper.py::num_active_valid:{num_active_valid} '
