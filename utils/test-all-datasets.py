678ec647f8b342e396a7f2a58e919fb7b09c2f25
LanceKnight <lanceknight26@gmail.com>
2022-05-18 13:41:44 -0500

    test_all_datasets.py:no need to test if results exist

--- utils/test-all-datasets.py
+++ utils/test-all-datasets.py
@@ -26,34 +26,35 @@ def run_command(dataset): # Change this
     cwd = os.getcwd()
     print(f'dataset:{dataset}')
     # Model=kgnn
-    os.system(f'python -W ignore entry.py \
-        --task_name {dataset}_test\
-        --dataset_name {dataset} \
-        --seed 26\
-        --num_workers 16 \
-        --dataset_path ../../../dataset/ \
-        --enable_oversampling_with_replacement \
-        --warmup_iterations 300 \
-        --max_epochs 20\
-        --peak_lr 5e-2 \
-        --end_lr 1e-9 \
-        --batch_size 17 \
-        --default_root_dir actual_training_checkpoints \
-        --gpus 1 \
-        --num_layers 3 \
-        --num_kernel1_1hop 10 \
-        --num_kernel2_1hop 20 \
-        --num_kernel3_1hop 30 \
-        --num_kernel4_1hop 50 \
-        --num_kernel1_Nhop 10 \
-        --num_kernel2_Nhop 20 \
-        --num_kernel3_Nhop 30 \
-        --num_kernel4_Nhop 50 \
-        --node_feature_dim 27 \
-        --edge_feature_dim 7 \
-        --hidden_dim 32\
-        --test\
-        ')
+    if not osp.exists('logs/test_result.log'):
+        os.system(f'python -W ignore entry.py \
+            --task_name {dataset}_test\
+            --dataset_name {dataset} \
+            --seed 26\
+            --num_workers 16 \
+            --dataset_path ../../../dataset/ \
+            --enable_oversampling_with_replacement \
+            --warmup_iterations 300 \
+            --max_epochs 20\
+            --peak_lr 5e-2 \
+            --end_lr 1e-9 \
+            --batch_size 17 \
+            --default_root_dir actual_training_checkpoints \
+            --gpus 1 \
+            --num_layers 3 \
+            --num_kernel1_1hop 10 \
+            --num_kernel2_1hop 20 \
+            --num_kernel3_1hop 30 \
+            --num_kernel4_1hop 50 \
+            --num_kernel1_Nhop 10 \
+            --num_kernel2_Nhop 20 \
+            --num_kernel3_Nhop 30 \
+            --num_kernel4_Nhop 50 \
+            --node_feature_dim 27 \
+            --edge_feature_dim 7 \
+            --hidden_dim 32\
+            --test\
+            ')
 
 
 def run(folder):
@@ -73,7 +74,7 @@ if __name__ == '__main__':
     mp.set_start_method('spawn')
     exp_dir = '/home/liuy69/projects/unified_framework/experiments/'
 
-    # Get a list of folders
+   # Get a list of folders
     folder_list = []
     for folder in os.listdir(exp_dir):
         if 'exp' in folder:
@@ -88,10 +89,11 @@ if __name__ == '__main__':
     os.makedirs(os.path.dirname(file_name), exist_ok=True)
     with open(file_name, 'w') as output_file:
         for folder in folder_list:
-            with open(osp.join(exp_dir,'kgnn/log'), 'r') as in_file:
-                line = in_file.readline()
-                print(line)
+            with open(osp.join(exp_dir,f'{folder}/kgnn/logs/test_result.log'), 'r') as in_file:
+                for line in in_file:
+                    output_file.write(line)
+                    print(line)
+                output_file.write('\n=======\n')
     end_time=time.time()
     run_time = end_time-start_time
     print(f'finish getting all test result: {run_time/3600:0.0f}h{(run_time)%3600/60:0.0f}m{run_time%60:0.0f}')
-
