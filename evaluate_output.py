70f0989dc813de0a1dc2c3f59a4016c71570c795
LanceKnight <lanceknight26@gmail.com>
2022-05-22 21:22:42 -0500

    done evaluate_output.py

--- evaluate_output.py
+++ evaluate_output.py
@@ -1,7 +1,7 @@
 import argparse
 import numpy as np
 
-from evaluation import calculate_ppv
+from evaluation import calculate_ppv, calculate_logAUC
 
 default_input_file='../examples/bcl/20_models/results/1798.RSR.1_32_005_025/independent0-4_monitoring0-4_number0.gz.txt'
 
@@ -15,16 +15,21 @@ label_list = []
 with open(args.predicted_file, 'r') as in_file:
 	for line in in_file:
 		components = line.split(',')
-		pred_sc = components[0]
-		label = components[1]
+		pred_sc = float(components[0])
+		label = int(components[1][0])
 		sc_list.append(pred_sc)
 		label_list.append(label)
 		# print(f'pred_sc:{pred_sc} label:{label}')
 sc_list = np.array(sc_list)
 label_list = np.array(label_list)
 
-res = calculate_ppv(label_list, sc_list)
-print(res)
+print(sc_list)
+print('\n')
+print(label_list)
+logAUC = calculate_logAUC(label_list, sc_list)
+print(f'logAUC:{logAUC}')
+ppv = calculate_ppv(label_list, sc_list)
+print(f'ppv:{ppv}')
 
 
 
