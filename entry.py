ea7b7c893c85e3b5458c8c18176ca7eb0f31a38a
Yunchao "Lance" Liu <lanceknight26@gmail.com>
2022-07-17 01:13:57 -0500

    use spherenet

--- entry.py
+++ entry.py
@@ -428,11 +428,11 @@ if __name__ == '__main__':
     Task.set_offline(offline_mode=True)
     # The reason that gnn_type cannot be a cmd line
     # argument is that model specific arguments depends on it
-    gnn_type = 'kgnn'
+    # gnn_type = 'kgnn'
     # gnn_type = 'dimenet' # Not implemented
     # gnn_type = 'chironet'
     # gnn_type = 'dimenet_pp'
-    # gnn_type = 'spherenet'
+    gnn_type = 'spherenet'
 
     print(f'========================')
     print(f'Runing model: {gnn_type}')
