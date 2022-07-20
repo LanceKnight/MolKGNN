685afc4b8f3b84210ac1660b6c67ee07eb62f806
Yunchao Liu <lanceknight26@gmail.com>
2022-07-20 16:45:24 -0500

    updated scheduler

--- utils/scheduler-ndslab3-kgnn.py
+++ utils/scheduler-ndslab3-kgnn.py
@@ -9,8 +9,8 @@ import time
 from datetime import datetime
 import math
 
-branch = 'chirality-last-layer' # Change this
-task_comment = '\"chirality only applies to the last layer\test of 1798\nadd-back chirality\nchirality_sign is for tot_sc\"' # Change this
+branch = 'optimize' # Change this
+task_comment = '\"optimize: mlp-before-pool; batch norm; learnable weights\"' # Change this
 
 def gitclone(dir_name):
     cwd = os.getcwd()
@@ -166,9 +166,9 @@ if __name__ == '__main__':
     seed_list = [1, 2, 3, 4, 10] # arg1
     warmup_list = [200] # arg2
     epochs_list = [20] # arg3
-    peak_lr_list = [5e-3] # arg4
+    peak_lr_list = [5e-2, 5e-3] # arg4
     end_lr_list = [1e-10] # arg5
-    num_layer_list = [4] # arg6
+    num_layer_list = [3,4] # arg6
     kernel1_list = [10] # arg7
     kernel2_list = [20] # arg8
     kernel3_list = [30] # arg9
