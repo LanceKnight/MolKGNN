f36c6d48d34ec5c182cd45df4fd7e90df5516bb2
Yunchao "Lance" Liu <lanceknight26@gmail.com>
2022-07-24 19:46:37 -0500

    add new data_split

--- utils/scheduler-live-kgnn.py
+++ utils/scheduler-live-kgnn.py
@@ -10,7 +10,7 @@ from datetime import datetime
 import math
 
 branch = 'shrink-no-valid' # Change this
-task_comment = '\"fullset-no-valid\"' # Change this
+task_comment = '\"shrink-no-valid\"' # Change this
 
 def gitclone(dir_name):
     cwd = os.getcwd()
@@ -95,7 +95,7 @@ def overwrite_dir(src, dst):
         else: raise
 
 def run(exp_id, *args):
-    exp_name = f'exp{exp_id}_{args[0]}_seed{args[1]}_fullsetnovalid_epoch{args[3]}_peak{args[4]}_end{args[5]}_layers{args[6]}_k1{args[7]}_k2{args[8]}_k3{args[9]}_k4{args[10]}_hidden{args[11]}_batch{args[12]}' # Change this
+    exp_name = f'exp{exp_id}_{args[0]}_seed{args[1]}_shrinknovalid_epoch{args[3]}_peak{args[4]}_end{args[5]}_layers{args[6]}_k1{args[7]}_k2{args[8]}_k3{args[9]}_k4{args[10]}_hidden{args[11]}_batch{args[12]}' # Change this
     print(f'=====running {exp_name}')
 
     # Go to correct folder
@@ -163,7 +163,7 @@ if __name__ == '__main__':
     # Hyperparms
     # dataset_list = ['435008', '1798', '435034', '1843', '2258', '463087', '488997','2689', '485290']
     dataset_list = [ '2258', '1798' ] # arg0
-    seed_list = [1, 2, 3, 4, 10] # arg1
+    seed_list = [2, 3, 4] # arg1
     warmup_list = [200] # arg2
     epochs_list = [13] # arg3
     peak_lr_list = [5e-3] # arg4
