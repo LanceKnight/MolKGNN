fd83babb76d1238ff3158489a6a52267310e0a81
Yunchao "Lance" Liu <lanceknight26@gmail.com>
2022-07-29 10:16:12 -0500

    use self-loop

--- utils/scheduler-live-kgnn.py
+++ utils/scheduler-live-kgnn.py
@@ -175,9 +175,9 @@ if __name__ == '__main__':
     seed_list = [1, 2, 10] # arg1
     warmup_list = [300] # arg2
     epochs_list = [20] # arg3
-    peak_lr_list = [5e-3] # arg4
+    peak_lr_list = [5e-2, 5e-3] # arg4
     end_lr_list = [1e-10] # arg5
-    num_layer_list = [4] # arg6
+    num_layer_list = [3, 4] # arg6
     kernel1_list = [10] # arg7
     kernel2_list = [20] # arg8
     kernel3_list = [30] # arg9
