4d047e0a4b1bb915cacb3e7224a2486979c2e194
Yunchao "Lance" Liu <lanceknight26@gmail.com>
2022-08-03 17:10:20 -0500

    added length_sc

--- models/KGNN/kernels.py
+++ models/KGNN/kernels.py
@@ -308,38 +308,38 @@ class KernelConv(Module):
         # print(f'get_angle():sc:{sc.shape}')
         # return sc.squeeze(1)
 
-    # def get_length_score(self, p_neighbor, p_support):
-    #     """
-    #     Compare the length of the neighbors and supports
-    #
-    #     It calculates the norm for all vectors in neighbors/supports first
-    #     and then calculates similarities between those norms
-    #     :param p_neighbor: Shape[num_nodes_in_this_degree, degree, dim]
-    #     :param p_support: Shape[num_kernels, num_nodes_in_this_degree,
-    #     degree, dim]
-    #     :return: a tensor. Shape[num_kernel, num_nodes_in_this_degree]
-    #     """
-    #     len_p_neighbor = torch.norm(p_neighbor, dim=-1)
-    #     len_p_support = torch.norm(p_support, dim=-1)
-    #
-    #
-    #
-    #     # Round the length of neighbors to 0.1. E.g., 1.512 becomes 1.5
-    #     # This eliminates the impact of small difference in lengths
-    #     len_p_neighbor = (len_p_neighbor * 10).round()/10
-    #
-    #     # Debug
-    #     # deg = p_neighbor.shape[-2]
-    #     # if deg == 4:
-    #     #     print(f'get_length():p_neighbor:{p_neighbor}')
-    #     #     print(f'get_length_score():p_neighbor:{p_neighbor.shape} p_support:'
-    #     #           f'{p_support.shape}')
-    #     #     print(f'get_length_score():\nlen_p_neighbor:\n'
-    #     #           f'{len_p_neighbor}\nlen_p_support:\n{len_p_support}')
-    #
-    #     # Get the similarity score
-    #     sc = self.calculate_average_similarity_score(len_p_neighbor, len_p_support, sim_dim=(-1))
-    #     return sc
+    def get_length_score(self, p_neighbor, p_support):
+        """
+        Compare the length of the neighbors and supports
+
+        It calculates the norm for all vectors in neighbors/supports first
+        and then calculates similarities between those norms
+        :param p_neighbor: Shape[num_nodes_in_this_degree, degree, dim]
+        :param p_support: Shape[num_kernels, num_nodes_in_this_degree,
+        degree, dim]
+        :return: a tensor. Shape[num_kernel, num_nodes_in_this_degree]
+        """
+        len_p_neighbor = torch.norm(p_neighbor, dim=-1)
+        len_p_support = torch.norm(p_support, dim=-1)
+
+
+
+        # Round the length of neighbors to 0.1. E.g., 1.512 becomes 1.5
+        # This eliminates the impact of small difference in lengths
+        len_p_neighbor = (len_p_neighbor * 10).round()/10
+
+        # Debug
+        # deg = p_neighbor.shape[-2]
+        # if deg == 4:
+        #     print(f'get_length():p_neighbor:{p_neighbor}')
+        #     print(f'get_length_score():p_neighbor:{p_neighbor.shape} p_support:'
+        #           f'{p_support.shape}')
+        #     print(f'get_length_score():\nlen_p_neighbor:\n'
+        #           f'{len_p_neighbor}\nlen_p_support:\n{len_p_support}')
+
+        # Get the similarity score
+        sc = self.calculate_average_similarity_score(len_p_neighbor, len_p_support, sim_dim=(-1))
+        return sc
 
     def get_the_permutation_with_best_alignment_id(self, input_tensor,
                                                    best_alignment_id):
--- utils/scheduler-live-kgnn.py
+++ utils/scheduler-live-kgnn.py
@@ -98,7 +98,7 @@ def overwrite_dir(src, dst):
         else: raise
 
 def run(exp_id, *args):
-    exp_name = f'exp{exp_id}idxline_{args[0]}_seed{args[1]}_warm{args[2]}_epoch{args[3]}_peak{args[4]}_layers{args[6]}_k1{args[7]}_k2{args[8]}_k3{args[9]}_k4{args[10]}_hidden{args[11]}_batch{args[12]}_decay{args[14]}_dropout{args[15]}' # Change this
+    exp_name = f'exp{exp_id}_{args[0]}_seed{args[1]}_warm{args[2]}_epoch{args[3]}_peak{args[4]}_layers{args[6]}_k1{args[7]}_k2{args[8]}_k3{args[9]}_k4{args[10]}_hidden{args[11]}_batch{args[12]}_decay{args[14]}_dropout{args[15]}' # Change this
     print(f'=====running {exp_name}')
 
     # Go to correct folder
